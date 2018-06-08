0d115a8d9ca31aa5a3ccbb33e4ac6ae5aa02e0f2
Lucas Nascimento <Lucas@localhost.localdomain>
2018-06-08 20:33:17 -0300

    Ultima 08/09/18

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,12 @@
  var modal=document.getElementById("modal"); 
         var btn=document.getElementById("btnModal");
         var span=document.getElementsByClassName("fechar")[0];
-        btn.onclick=function(){
-            modal.style.display="block";
-        }
-        span.onclick=function(){
-            modal.style.display="none";
-        }
-        window.onclick=function(event){
-            if(event.target==modal){
-                modal.style.display="none";
-            }
-        }
+        elemento=document.getElementsByClassName('modal-content')[0];
+    btn.onclick=function(){
+        modal.style.display="block";
+    }
+    span.onclick=function(){
+        elemento.style.animationName ="decer";
+        setTimeout(function(){modal.style.display="none"},1000);
+        
+    }
